fad58e816564bc3ae851337e022fd9822afe0668
Sylvia <ndilisylvia@gmail.com>
2022-09-06 15:24:26 +0300

    add your music list from the db and load your track index

--- index.js
+++ index.js
@@ -19,3 +19,68 @@ let track_index = 0;
 let isPlaying = false;
 let isRandom = false;
 let updateTimer
+
+const music_list = [
+    {
+        artist: "Harry Styles",
+        name: "Watermelon Sugar",
+        music: "/music/Watermelon_Sugar.mp3",
+        img: "/images/Watermelon-sugar.jpg"
+      },
+      {
+        artist: "Hozier",
+        name: "Take Me To Church",
+        music: "/music/Take_Me_To_Church.mp3",
+        img: "/images/Take-me-to-church.jpg"
+      },
+      {
+        artist: "Alec Benjamin",
+        name: "Devil Doesn't Bargain",
+        music: "/music/Devil_Doesn't_Bargain.mp3",
+        img: "/images/Devil-Doesn't-Bargain.jpg"
+      },
+      {
+        artist: "Troye Sivan",
+        name: "Angel Baby",
+        music: "/music/Angel_Baby.mp3",
+        img: "/images/Angel-Baby.jpg"
+      },
+      {
+        artist: "Elley Duhe",
+        name: "Middle of the Night",
+        music: "/music/Middle_of_the_Night.mp3",
+        img: "/images/Middle-of-the-Night.jpg"
+      },
+      {
+        artist: "SAINT JHN",
+        name: "The best part of life",
+        music: "/music/THE_BEST_PART_OF_LIFE.mp3",
+        img: "/images/The-best-part-of-life.jpg"
+      },
+      {
+        artist: "Forrest Frank",
+        name: "Slow Down",
+        music: "/music/slow_down.mp3",
+        img: "/images/Slow-Down.jpg"
+      },
+      {
+        artist: "RUTH B",
+        name: "Dandelions",
+        music: "/music/Dandelions.mp3",
+        img: "/images/Dandelions.jpg"
+      },
+      {
+        artist: "Chris Brown",
+        name: "Under the influence",
+        music: "/music/Under_The_Influence.mp3",
+        img: "/images/Under-the-influence.jpg"
+      },
+      {
+        artist: "JP Cooper",
+        name: "In these arms",
+        music: "/music/In_These_Arms.mp3",
+        img: "/images/In-these-arms.jpg"
+      },
+];
+
+loadTrack(track_index);
